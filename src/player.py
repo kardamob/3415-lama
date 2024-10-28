48446896455aa56ee191fb90f78dde6bc4d80934
Дима <dimakowy1@yandex.ru>
2024-10-28 12:33:02 +0300

    class player + tests

---
+++ src/player.py
@@ -0,0 +1,39 @@
+import hashlib
+import json
+import typing
+
+from src.hand import Hand
+
+
+class Player:
+    def __init__(self, name: str, hand: Hand, score: int = 0):
+        self.name = name
+        self.hand = hand
+        self.score = score
+
+    def __str__(self):
+        return f"{self.name}({self.score}): {self.hand}"
+
+    def __eq__(self, other: typing.Self | str | dict):
+        if isinstance(other, str):
+            other = self.load(json.loads(other))
+        if isinstance(other, dict):
+            other = self.load(other)
+        return (
+            self.name == other.name
+            and self.score == other.score
+            and self.hand == other.hand
+        )
+
+    def __hash__(self) -> int:
+        # Can't "return self.name", because hash must return an integer
+        return int(hashlib.sha1(self.name.encode("utf-8")).hexdigest(), 16) % (10**8)
+
+    def save(self) -> dict:
+        return {"name": self.name, "hand": str(self.hand), "score": self.score}
+
+    @classmethod
+    def load(cls, data: dict):
+        return cls(
+            name=data["name"], hand=Hand.load(data["hand"]), score=int(data["score"])
+        )
---
+++ test/test_player.py
@@ -0,0 +1,38 @@
+from src.hand import Hand
+from src.player import Player
+
+
+def test_init():
+    h = Hand.load("1 3 2")
+    p = Player(name="Alex", hand=h, score=10)
+    assert p.name == "Alex"
+    assert p.hand == h
+    assert p.score == 10
+
+
+def test_str():
+    h = Hand.load("1 3 2")
+    p = Player(name="Alex", hand=h, score=15)
+    assert str(p) == "Alex(15): 1 3 2"
+
+
+def test_save():
+    h = Hand.load("1 3 2")
+    p = Player(name="Alex", hand=h, score=15)
+    assert p.save() == {"name": "Alex", "score": 15, "hand": "1 3 2"}
+
+
+def test_eq():
+    h1 = Hand.load("1 3 2")
+    h2 = Hand.load("1 3 2")
+    p1 = Player(name="Alex", hand=h1, score=10)
+    p2 = Player(name="Alex", hand=h2, score=10)
+    assert p1 == p2
+
+
+def test_load():
+    data = {"name": "Alex", "score": 10, "hand": "1 3 2"}
+    h = Hand.load("1 3 2")
+    p_expected = Player(name="Alex", hand=h, score=10)
+    p = Player.load(data)
+    assert p == p_expected
