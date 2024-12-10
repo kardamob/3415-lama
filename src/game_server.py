85ec903ceb1559f56496af626e8c6fa9e0eed435
Дима <dimakowy1@yandex.ru>
2024-12-10 03:16:48 +0300

    GamePhase добавление фаз

--- src/game_server.py
+++ src/game_server.py
@@ -10,10 +10,13 @@ from src.hand import Hand
 from src.player import Player
 
 class GamePhase(enum.StrEnum):
+    GAME_START = "Game starts"
     CHOOSE_CARD = "Choose card"
     DRAW_EXTRA = "Draw extra card"
     CHOOSE_CARD_AGAIN = "Choose card again"
     NEXT_PLAYER = "Switch current player"
+    PLAYER_QUIT = "Player ends his turn"
+    BEGIN_ROUND = "Round begins"
     END_ROUND = "Round ended"
     DECLARE_WINNER = "Declare a winner"
     GAME_END = "Game ended"
